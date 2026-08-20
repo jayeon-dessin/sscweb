9c5697a5dd2949d0454cddfe40729f9bd816a486
jayeon-dessin <leecm0070@gmail.com>
2026-08-20 23:33:39 +0900

    fix

--- album_cover_downloader_bundle/run_mac_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python3 download_album_covers.py 'sscdbg(2).json'
-printf '\nPress Enter to close...\n'
-read x
--- album_cover_downloader_bundle/run_windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-where py >nul 2>nul
-if %errorlevel%==0 (
-  py -3 download_album_covers.py "sscdbg(2).json"
-) else (
-  python download_album_covers.py "sscdbg(2).json"
-)
-echo.
-pause
