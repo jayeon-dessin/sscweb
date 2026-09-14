29d28b4fcab144a8b928126539379b3ad649ea6b
jayeon-dessin <leecm0070@gmail.com>
2026-09-15 01:06:15 +0900

    .

--- bubble.js
+++ bubble.js
@@ -93,8 +93,8 @@ function computeSimilarity(songA, songB) {
   const langSim = jaccardSimilarity(songA.language, songB.language);
 
   return (
-    geoSim * 0.4 +
-    tagSim * 0.2 +
+    geoSim * 0.3 +
+    tagSim * 0.3 +
     artistSim * 0.2 +
     writerSim * 0.1 +
     langSim * 0.1
